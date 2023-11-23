d958c32c7f3c55081af01a4608170798255c4ae1
maximumry <maximum.river1211@gmail.com>
2023-11-23 14:32:52 +0900

    Initial commit

---
+++ src/main/java/in/techcamp/issueapp1/IssueApp1Application.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/in/techcamp/issueapp1/IssueApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.issueapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
