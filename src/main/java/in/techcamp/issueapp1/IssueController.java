0fbf1e9248397bc9b0ca439ed69c0be352fecf74
maximumry <maximum.river1211@gmail.com>
2023-11-25 15:17:08 +0900

    一覧表示の実装

--- src/main/java/in/techcamp/issueapp1/IssueController.java
+++ src/main/java/in/techcamp/issueapp1/IssueController.java
@@ -1,7 +1,8 @@
-package in.techcamp.issueapp1;
+package in.techcamp.issueapp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,14 +10,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 @AllArgsConstructor
 public class IssueController {
+
     private final IssueRepository issueRepository;
+
     @GetMapping("/issueForm")
     public String showIssueForm(@ModelAttribute("issueForm") IssueForm form){
         return "issueForm";
     }
+
     @PostMapping("/issues")
     public String createIssue(IssueForm issueForm){
         issueRepository.insert(issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String showIssues(Model model){
+        var issueList = issueRepository.findAll();
+        model.addAttribute("issueList", issueList);
+        return "index";
     }
 }
--- src/main/java/in/techcamp/issueapp1/IssueEntity.java
+++ src/main/java/in/techcamp/issueapp1/IssueEntity.java
@@ -1,9 +1,12 @@
 package in.techcamp.issueapp;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
-public class IssueForm {
+public class IssueEntity {
+    private long id;
     private String title;
     private String content;
     private String period;
