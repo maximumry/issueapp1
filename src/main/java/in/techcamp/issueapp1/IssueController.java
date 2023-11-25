83b573875f9402b700855f4aeed7125e30f8042d
maximumry <maximum.river1211@gmail.com>
2023-11-25 19:18:22 +0900

    アップデートの実装

--- src/main/java/in/techcamp/issueapp1/IssueController.java
+++ src/main/java/in/techcamp/issueapp1/IssueController.java
@@ -1,10 +1,12 @@
 package in.techcamp.issueapp;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -29,5 +31,18 @@ public class IssueController {
         var issueList = issueRepository.findAll();
         model.addAttribute("issueList", issueList);
         return "index";
+    }
+
+    @GetMapping("/issues/{id}")
+    public String issueDetail(@PathVariable long id, Model model){
+        var issue = issueRepository.findById(id);
+        model.addAttribute("issue", issue);
+        return "detail";
+    }
+
+    @PostMapping("/issues/{id}/update")
+    public String updateissue(@PathVariable long id, IssueForm issueForm){
+        issueRepository.update(id, issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
+        return "redirect:/";
     }
 }
